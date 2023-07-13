0e6b7a1f64771990b9f2f7dcabcc2f751f9af675
Adarsh-kuiya <115663933+Adarsh-kuiya@users.noreply.github.com>
2023-07-13 18:53:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function f(str){
+    let len = str.length;
+    for(let i =0; i< len; i++){
+        let currentCharacter = str.charAt(i);
+        let count =0;
+        for(let j =0; j<len;j++){
+            let strIterationCharacter = str.charAt(j);
+              if(currentCharacter==strIterationCharacter)count++;
+        }
+        if(count==1)return currentCharacter;
+    }
+    return null;
+    }
+
+var str = prompt();
+// alert(f(str))
